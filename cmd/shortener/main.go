9b8b072763e7a1c8f6ca27afed7502577bdc8b0a
Alexey Samoylov <alexey.samoylov@gmail.com>
2021-08-24 17:35:16 +0700

    switch to chi router

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/russianlagman/go-musthave-shortener/internal/app"
 	"log"
 	"net/http"
@@ -10,7 +12,10 @@ import (
 func main() {
 	addr := "localhost:8080"
 	shortener := app.NewMemoryShortenerService(addr)
-	http.HandleFunc("/", app.RouterHandler(shortener))
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/{id:[0-9a-z]+}", app.ReadHandler(shortener))
+	r.Post("/", app.WriteHandler(shortener))
 	fmt.Printf("Listening on %s\n", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.Fatal(http.ListenAndServe(addr, r))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/russianlagman/go-musthave-shortener
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/go-chi/chi/v5 v5.0.3 // indirect
+	github.com/stretchr/testify v1.7.0
+)
