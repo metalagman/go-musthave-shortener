897f3c887d7cf2045856d42db0b471a75ced2ef4
Alexey Samoylov <alexey.samoylov@gmail.com>
2021-09-15 09:44:20 +0700

    go vet feedback

--- internal/app/middleware/auth/auth.go
+++ internal/app/middleware/auth/auth.go
@@ -10,39 +10,39 @@ import (
 	"time"
 )
 
-const uidCookieName string = "uid"
+const cookieNameUID string = "uid"
 
-type ContextKeyUid struct{}
+type ContextKeyUID struct{}
 
 func SecureCookie(secretKey string) func(next http.Handler) http.Handler {
 	sc := securecookie.New(secretKey)
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			log.Printf("auth middleware")
 			var uid string
 			var err error
 
 			// reading or regenerating uid
-			uid, err = readUid(r, sc)
+			uid, err = readUID(r, sc)
 			if err != nil {
-				uid, err = regenerateUid(w, sc)
+				log.Printf("auth read error: %v", err)
+				uid, err = regenerateUID(w, sc)
 				if err != nil {
-					log.Printf("auth middleware error: %v", err)
+					log.Printf("auth regenerate error: %v", err)
 					next.ServeHTTP(w, r)
 					return
 				}
 			}
 
 			// inject uid into context
-			ctx := context.WithValue(r.Context(), ContextKeyUid{}, uid)
+			ctx := context.WithValue(r.Context(), ContextKeyUID{}, uid)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
 }
 
-// readUid from request cookie and store it into a new context
-func readUid(r *http.Request, sc *securecookie.SecureCookie) (string, error) {
-	cookie, err := r.Cookie(uidCookieName)
+// readUID from request cookie and store it into a new context
+func readUID(r *http.Request, sc *securecookie.SecureCookie) (string, error) {
+	cookie, err := r.Cookie(cookieNameUID)
 	if err != nil {
 		return "", fmt.Errorf("cookie read: %w", err)
 	}
@@ -52,13 +52,13 @@ func readUid(r *http.Request, sc *securecookie.SecureCookie) (string, error) {
 	return cookie.Value, nil
 }
 
-// regenerateUid and send it within the cookie
-func regenerateUid(w http.ResponseWriter, sc *securecookie.SecureCookie) (string, error) {
+// regenerateUID and send it within the cookie
+func regenerateUID(w http.ResponseWriter, sc *securecookie.SecureCookie) (string, error) {
 	uid := uuid.New().String()
 
 	expiration := time.Now().Add(365 * 24 * time.Hour)
 	cookie := &http.Cookie{
-		Name:    uidCookieName,
+		Name:    cookieNameUID,
 		Value:   uid,
 		Expires: expiration,
 	}
