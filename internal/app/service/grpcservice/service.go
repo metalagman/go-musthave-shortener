32ac9ed4d2b7d2dd4a109479066423c6a31aa1df
Alexey Samoylov <alexey.samoylov@gmail.com>
2022-04-20 16:58:40 +0700

    added method stubs

--- internal/app/service/grpcservice/service.go
+++ internal/app/service/grpcservice/service.go
@@ -10,6 +10,31 @@ type ShortenerService struct {
 	pb.UnimplementedShortenerServer
 }
 
+func (s *ShortenerService) Shorten(ctx context.Context, request *pb.ShortenRequest) (*pb.ShortenResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *ShortenerService) BatchShorten(ctx context.Context, request *pb.BatchShortenRequest) (*pb.BatchShortenResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *ShortenerService) Expand(ctx context.Context, request *pb.ExpandRequest) (*pb.ExpandResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *ShortenerService) BatchRemove(ctx context.Context, request *pb.BatchRemoveRequest) (*pb.BatchRemoveResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *ShortenerService) mustEmbedUnimplementedShortenerServer() {
+	//TODO implement me
+	panic("implement me")
+}
+
 func NewShortenerService() *ShortenerService {
 	s := &ShortenerService{}
 
